ea2a7228b5fb12c610f6edab21e834305f5f2033
peachpieproductions <benjames.im@gmail.com>
2019-04-05 13:54:56 -0400

    bens workbb

---
+++ TheThreeVirtualTour/Assets/Scripts/ColorPicker.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorPicker : MonoBehaviour
+{
+
+    public Camera cam;
+    public Color startingColor = Color.white;
+    public Material material;
+    public Texture2D colorWheelTex;
+    public Transform cursor;
+    public float texSize;
+    public Color testColor;
+    public Transform origin;
+    public Vector2 cursorPosition;
+    public bool open;
+    float openTimer;
+
+    private void Start() {
+        texSize = GetComponent<BoxCollider>().size.x;
+        transform.localScale = Vector3.one * (open ? 1f : .2f);
+        material.color = startingColor;
+    }
+
+    private void Update() {
+
+        //Look at camera
+        transform.LookAt(cam.transform);
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+
+        transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one * (open? 1f : .2f), Time.deltaTime * 5f);
+        
+        if (open) {
+            openTimer += Time.deltaTime;
+            if (openTimer > 3.5f) {
+                open = false;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0)) {
+
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray,  out RaycastHit hit)) {
+                var picker = hit.transform.GetComponent<ColorPicker>();
+                if (picker && picker == this) {
+
+                    openTimer = 0;
+                    if (!open) {
+                        open = true;
+                        return;
+                    }
+
+                    cursorPosition = Random.insideUnitCircle;
+
+                    picker.cursor.position = hit.point;
+
+                    cursorPosition = picker.cursor.localPosition * 1.25f;
+
+                    var newCol = colorWheelTex.GetPixel(Mathf.RoundToInt(((cursorPosition.x + 1) / 2) * colorWheelTex.width),
+                        Mathf.RoundToInt(((cursorPosition.y + 1) / 2) * colorWheelTex.height));
+                    if (newCol.a == 1) testColor = newCol;
+                    cursor.transform.localPosition = new Vector3((texSize / 2) * cursorPosition.x, (texSize / 2) * cursorPosition.y);
+
+                    if (material) {
+                        material.color = testColor;
+                    }
+                }
+            }
+            
+            
+        }
+    }
+
+
+}
---
+++ TheThreeVirtualTour/Assets/Scripts/InteractableOpen.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableOpen : MonoBehaviour
+{
+
+    public bool open;
+    public float openSpeed = 1f;
+    public Vector3 openAngleOffset;
+    public Material hoverMaterial;
+    Vector3 startingAngle;
+    List<MeshRenderer> meshRenderers = new List<MeshRenderer>();
+    List<Material> originalMaterials = new List<Material>();
+
+    private void Start() {
+        startingAngle = transform.localEulerAngles;
+        foreach(MeshRenderer mr in transform.GetComponentsInChildren<MeshRenderer>()) {
+            meshRenderers.Add(mr);
+            originalMaterials.Add(mr.material);
+        }
+    }
+
+    private void Update() {
+        
+        if (open) {
+            transform.localEulerAngles = Vector3.Slerp(transform.localEulerAngles, startingAngle + openAngleOffset, Time.deltaTime * openSpeed);
+        } else {
+            transform.localEulerAngles = Vector3.Slerp(transform.localEulerAngles, startingAngle, Time.deltaTime * openSpeed);
+        }
+
+    }
+
+    private void OnMouseDown() {
+        open = !open;
+    }
+
+    private void OnMouseEnter() {
+        if (hoverMaterial) {
+            foreach (MeshRenderer mr in meshRenderers) {
+                mr.material = hoverMaterial;
+            }
+        }
+    }
+
+    private void OnMouseExit() {
+        if (hoverMaterial) {
+            int i = 0;
+            foreach (MeshRenderer mr in meshRenderers) {
+                mr.material = originalMaterials[i];
+                i++;
+            }
+        }
+    }
+
+
+}
